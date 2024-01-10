50cb8191d4b23e1480f21bc04d10f86218ff0c1b
Dinuka Perera <dsp068@student.bham.ac.uk>
2024-01-10 19:02:32 +0000

    added filtering

--- ssa-backend/app.py
+++ ssa-backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import yaml
-#from datetime import datetime
 
 app = Flask(__name__)
 db = yaml.load(open("db.yaml"), Loader=yaml.FullLoader)
@@ -15,6 +14,7 @@ class Supervisors(db.Model):
     supervisorName = db.Column(db.String(100), nullable=False)
     supervisorEmail = db.Column(db.String(200), nullable=False, unique=True)
     project_keywords = db.Column(db.Text)
+    filter_words = db.Column(db.Text)
     def __repr__(self):
         return '<Name %r>' %self.supervisorName
      
@@ -24,14 +24,28 @@ def index():
     #code for dashboard
     return "todo"
 
-@app.route('/supervisor-profiles')
+@app.route('/supervisor-profiles', methods=['GET'])
 def display_profiles():
     supervisors = Supervisors.query.all()
     output = []
     for supervisor in supervisors:
-        supervisor_data = {"name": supervisor.supervisorName, "email": supervisor.supervisorEmail, "projects":supervisor.project_keywords}
+        supervisor_data = {"name": supervisor.supervisorName, "email": supervisor.supervisorEmail, "projects":supervisor.project_keywords, "filter_words":supervisor.filter_words}
         output.append(supervisor_data)
     return jsonify({"supervisors": output})
+
+@app.route('/supervisor-filters', methods=['GET']) 
+def display_filters():
+    supervisors = Supervisors.query.all()
+    output = []
+    filter_list = []
+    for supervisor in supervisors:
+        unique_filters = supervisor.filter_words.split(',')
+        for filters in unique_filters:
+            filter_list.append(filters)
+    filter_list = list(set(filter_list))
+    for item in filter_list:
+        output.append(item)
+    return jsonify({"allFilters": output})
 
 if __name__ == "__main__":
     app.run(debug=True) #changes are updated immediately - set to False once in production
--- ssa-frontend/src/components/SupervisorProfiles.js
+++ ssa-frontend/src/components/SupervisorProfiles.js
@@ -1,43 +1,94 @@
 import React, { useState, useEffect } from "react"
 import "../App.css"
-import SearchBar from "./SearchBar"
 import {FaSearch} from "react-icons/fa"
 
 function SupervisorProfiles() {
-    const [supervisors, setSupervisors] = useState([{}]);
+  const [supervisors, setSupervisors] = useState([{}]);
 
-    const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetch("/supervisor-profiles").then(
-        res => res.json()
-        ).then(
-        data => {
-            setSupervisors(data.supervisors)
-            console.log(data)
-        }
-        )
-    }, [])
+  const [allFilters, setAllFilters] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredSupervisors, setFilteredSupervisors] = useState([{}]);
 
-    return (
-        <div className="page-content">
-        
-          <h1 className="page-title">Supervisor Profiles</h1>
+  useEffect(() => {
+      fetch("/supervisor-profiles").then(
+      res => res.json()
+      ).then(
+      data => {
+          setSupervisors(data.supervisors)
+          setFilteredSupervisors(data.supervisors)
+      }
+      )
+  }, [])
 
-          <div className="search-bar-container">
-            <div className="search-bar-div">
-                <FaSearch id="search-icon" />
-                <input  className="search-bar-input" 
-                        placeholder="Type to search..."
-                        value={searchTerm}
-                        onChange={(e) => {setSearchTerm(e.target.value);}}/>
-            </div>
+  useEffect(() => {
+    fetch("/supervisor-filters").then(
+    res => res.json()
+    ).then(
+    data => {
+        setAllFilters(data.allFilters)
+    }
+    )
+  }, [])
+
+  useEffect(() => {
+    filterSupervisors();
+  }, [selectedFilters])
+
+  const filterSupervisors = () => {
+    if (selectedFilters.length > 0) {
+      let tempSupervisors = selectedFilters.map((selectedCategory) => {
+        let temp = supervisors.filter((supervisor) => supervisor.filter_words.includes(selectedCategory))
+        return temp
+      });
+      setFilteredSupervisors(tempSupervisors.flat())
+    } else {
+      setFilteredSupervisors([...supervisors])
+    }
+    
+  }
+
+  const handleFilterClick = (selectedCategory) => {
+    if (selectedFilters.includes(selectedCategory)) {
+      setSelectedFilters(selectedFilters.filter((term) => term !== selectedCategory));
+    } else {
+      setSelectedFilters([...selectedFilters, selectedCategory]);
+    }
+ };
+
+  return (
+      <div className="page-content">
+      
+        <h1 className="page-title">Supervisor Profiles</h1>
+
+        <div className="search-bar-container">
+          <div className="search-bar-div">
+            <FaSearch id="search-icon" />
+            <input  className="search-bar-input" 
+                    placeholder="Type to search..."
+                    value={searchTerm}
+                    onChange={(e) => {setSearchTerm(e.target.value);}}/>
           </div>
+        </div>
 
-          <div className="supervisor-boxes">
-            {supervisors.filter((supervisor) => {
+        <div className="filter-container">
+          <p className="filter-by-text">Filter by:</p>
+          <div className="filter-buttons-div">
+            {allFilters.map((filterCategory, idx) => (
+                <button
+                  key={`filters-${idx}`}
+                  className={`filter-button ${selectedFilters?.includes(filterCategory) ? "active" : "" }`}
+                  onClick={() => handleFilterClick(filterCategory)}>
+                  {filterCategory} 
+                </button>
+              ))}
+          </div>
+        </div>
+
+        <div className="supervisor-boxes">
+          {filteredSupervisors.filter((supervisor) => {
                 if (searchTerm == "") {
-                    console.log(supervisor)
                     return supervisor
                 } else if (
                     supervisor.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -47,16 +98,16 @@ function SupervisorProfiles() {
                     return supervisor
                 }
             }
-            ).map((supervisor) => (
-              <div key={supervisor.name} className="supervisor-summary">
-                <h4 className="supervisor-name">{supervisor.name}</h4>
-                <p className="supervisor-email">{supervisor.email}</p>
-                <p className="supervisor-projects">{supervisor.projects}</p>
-              </div>
-            ))}
-          </div>
+          ).map((supervisor) => (
+            <div key={supervisor.name} className="supervisor-summary">
+              <h4 className="supervisor-name">{supervisor.name}</h4>
+              <p className="supervisor-email">{supervisor.email}</p>
+              <p className="supervisor-projects">{supervisor.projects}</p>
+            </div>
+          ))}
         </div>
-    )
+      </div>
+  )
 }
 
 export default SupervisorProfiles
